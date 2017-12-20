cd0322655326de3f59ba1db1b629d12c10639eb0
lavik424 <lavi424@gmail.com>
2017-12-20 21:32:56 +0200

    its been a while

--- csvJoin.py
+++ csvJoin.py
@@ -1,25 +1,71 @@
 import csv
+import numpy as np
 
-reads_list = [32270428,6403667,6220977,1605853,1943338,4432151,3828145,4567791,3907105,2225135,2990136,3417783,2629323,1499026,3295452,4465269, \
-              2751426,1034065,1420977,1545798]
 
-f1 = open('/home/lavik/Desktop/lavi/regenerationProject/dataFromBioMart', 'r', newline='')
-reader1 = csv.reader(f1)
-f2 = open('/home/lavik/Desktop/lavi/regenerationProject/originalDataFromDespina', 'r', newline='')
-reader2 = csv.reader(f2)
-with open('/home/lavik/Desktop/lavi/regenerationProject/result.csv', 'w', newline='') as f_des:
-    des = csv.writer(f_des, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for row1 in reader1:
-        for row2 in reader2:
-            val_list = []
-            for i in range(1,21):
-                val_list.append(int(row2[i])/reads_list[i-1])
-            if row1[0] == row2[0]:
-                row1[1] = row1[1].replace(',', '')
-                row_res = row1 + row2[1:] + val_list
+
+
+trashold = i/1034065
+
+reads_list = [1605853,1943338,4432151,3828145,4567791,
+              3907105,2225135,2990136,3417783,2629323,
+              1499026,3295452,4465269,32270428,2751426,
+              1034065,1420977,6403667,1545798,6220977]
+
+# merge original data table and data from BioMart. Also normalize num of reads
+def join2csv(reader1):
+    with open('/home/lavik/Desktop/lavi/regenerationProject/resultCPM.csv', 'w', newline='') as f_des:
+        des = csv.writer(f_des, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        minimumNumOfReads = min(reads_list)
+        for row1 in reader1:
+            for row2 in reader2:
+                val_list = []
+                for i in range(1, 21):
+                    value = int(row2[i]) / reads_list[i - 1]
+                    val_list.append((value*10**6) if value > minimum else 0)
+                if row1[0] == row2[0]:
+                    row1[1] = row1[1].replace(',', '')
+                    row_res = row1 + row2[1:] + val_list
+                    des.writerow(row_res)
+                    break
+                row_res = [row2[0], 'NULL', 'NULL'] + row2[1:] + val_list
                 des.writerow(row_res)
-                break
-            row_res = row2 + ['NULL','NULL'] + val_list
-            des.writerow(row_res)
+
+
+# removes the lines where all normalized value are 0
+def remove_zero_lines(reader):
+    with open('/home/lavik/Desktop/lavi/regenerationProject/result1withour0lines.csv', 'w', newline='') as f_des:
+        des = csv.writer(f_des, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in reader:
+            for value in row:
+                if float(value) > 0:
+                    des.writerow(row)
+                    break
+
+#flip normalized values to create vectors of different cells
+def flip_create_array(reader):
+    pass
+
+
+
+
+
+# main
+# f1 = open('/home/lavik/Desktop/lavi/regenerationProject/dataFromBioMart', 'r', newline='')
+# reader1 = csv.reader(f1)
+# f2 = open('/home/lavik/Desktop/lavi/regenerationProject/originalDataFromDespina', 'r', newline='')
+# reader2 = csv.reader(f2)
+# join2csv(reader1,reader2)
+# f1.close()
+# f2.close()
+f1 = open('/home/lavik/Desktop/lavi/regenerationProject/onlyCPM.csv', 'r', newline='')
+reader = csv.reader(f1)
+# x = list(reader)
+# result = np.array(x).astype(float)
+# for i in range(10):
+#     print(result[i])
+remove_zero_lines(reader)
 f1.close()
-f2.close()
+
+
+
+
--- noramlizeData.py
+++ noramlizeData.py
@@ -2,41 +2,25 @@ import csv
 from numpy import genfromtxt
 
 
+NUMOFTHERSHOLD = 20
 
 readsList = [1605853,1943338,4432151,3828145,4567791,
               3907105,2225135,2990136,3417783,2629323,
               1499026,3295452,4465269,32270428,2751426,
               1034065,1420977,6403667,1545798,6220977]
 
-# read original data and normalize it by CPM (Counts Per Million reads)
-# create 10 different output files each with different threshold i/min(num of reads in the samples)
-# def NormalizeDataByThershold(reader):
-#     minimumNumOfReads = min(readsList)
-#     for i in range(1,11):
-#         thershold = i/minimumNumOfReads
-#         with open('/home/lavik/Desktop/lavi/regenerationProject/CPMThershold{}.csv'.format(i), 'w', newline='') as f_des:
-#             des = csv.writer(f_des, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#             for row in reader:
-#                 # valList = [(int(row[i]) / reads_list[j]) for j in range(len(row))]
-#                 valList = []
-#                 for j in range(len(row)):
-#                     print(row[j])
-#                     value = int(row[j]) / readsList[j]
-#                     valList.append((value * 10 ** 6) if value > thershold else 0)
-#                 des.writerow(valList)
-
 
 # reads original data and normalize it by CPM (Counts Per Million reads)
 # creates 10 different output files each with different threshold i/min(num of reads in the samples)
 def NormalizeDataByThershold(filePath):
     minimumNumOfReads = min(readsList)
-    for i in range(1, 11):
+    for i in range(1, NUMOFTHERSHOLD):
         print("starting {} iteration".format(i))
         thershold = i / minimumNumOfReads
         destinationPath = '/home/lavik/Desktop/lavi/regenerationProject/CPMThershold{}.csv'.format(i)
         with open(destinationPath, 'w', newline='') as f_des:
             des = csv.writer(f_des, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            data = genfromtxt(filePath, delimiter=',', dtype='U')
+            data = genfromtxt(filePath, delimiter=',', dtype='U', skip_header=1)
             for row in range(len(data)):
                 valList = [data[row][0]]
                 for j in range(1,len(data[row])):
@@ -51,6 +35,9 @@ def removeZeroLines(reader,filePath):
     print("removing zero lines")
     with open(filePath, 'w', newline='') as f_des:
         des = csv.writer(f_des, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        row = "GeneID	Sca1+D0	Sca+D1	Sca+D3	Sca+D7	Sca+D30	K15+D0	K15+D1	K15+D3	K15+D7	K15+D30	K15+/CD34+D0" \
+              "	K15+/CD34+D1	K15+/CD34+D3	K15+/CD34+D7	K15+/CD34+D30	CD34+D0	CD34+D1	CD34+D3	CD34+D7	CD34+D30"
+        des.writerow(row.split("\t"))
         for row in reader:
             for value in row[1:]:
                 if float(value) > 0:
@@ -63,7 +50,7 @@ def removeZeroLines(reader,filePath):
 # reader = csv.reader(f1)
 filePath = '/home/lavik/Desktop/lavi/regenerationProject/originalSortedCols.csv'
 NormalizeDataByThershold(filePath)
-for i in range(1,11):
+for i in range(1,NUMOFTHERSHOLD):
     f1 = open('/home/lavik/Desktop/lavi/regenerationProject/CPMThershold{}.csv'.format(i), 'r', newline='')
     reader = csv.reader(f1)
     filePath = '/home/lavik/Desktop/lavi/regenerationProject/CPMNoZeroLinesThershold{}.csv'.format(i)
